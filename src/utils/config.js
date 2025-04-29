a101c42b7601fb13bff20be5083c545d8c7d0e33
AbhiDwivedi <abhidwivedi687@gmail.com>
2025-04-29 22:29:10 +0530

    update the url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,25 +1,19 @@
 const stripe_sky = 'pk_test_51Nk8Y4F0B89ncn3xWB6ZN3GsbVIVL7Jqfa3jxtIOpPkKHcleHZw4EMPJKd4cRwm34ZARBeYmAWwu3VxyYL1gb6OT00UKNSvfvb'
 
 
-// Comment out production settings for now
-// const production = 'production'
-// const dev = 'development'
+const production = 'production'
+const dev = 'development'
 
-// const mode = production
-
-// Set mode to 'development' directly
-const mode = 'development'
-
-
+const mode = production
 
 let app_url, api_url
 
-if (mode === "production") {
-    // app_url = "https://shopmy-cyan.vercel.app"
-    // api_url = "https://api-ecommerce-fos4.onrender.com"
+if (mode === production) {
+    app_url = "https://multi-vendor-frontend.vercel.app/"
+    api_url = "https://multi-vendor-backend-l3yd.onrender.com"
 } else {
-    app_url = 'http://localhost:3000'  // Frontend URL
-    api_url = 'http://localhost:5000'  // Backend URL
+    app_url = 'http://localhost:3000'
+    api_url = 'http://localhost:5000'
 }
 
 export {
